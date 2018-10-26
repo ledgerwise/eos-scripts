751a07ca5f1d04e733c11784c1e400b157e10bbf
Jorge Ferrando <jorfermo@gmail.com>
2018-10-26 10:12:23 +0200

    Update eoslpb.py

--- eoslpb.py
+++ eoslpb.py
@@ -28,13 +28,13 @@ def main():
     endpoints = options.endpoints.split(',')
     network = options.network
 
-    json_file = '{}.lbp.json'.format(network)
+    json_file = '{}.lpb.json'.format(network)
     try:
-        eoslbp = mpu.io.read(json_file)
+        eoslpb = mpu.io.read(json_file)
     except:
-        eoslbp = {
+        eoslpb = {
         }
-        mpu.io.write(json_file, eoslbp)
+        mpu.io.write(json_file, eoslpb)
 
     while True:
         for endpoint in endpoints: 
@@ -44,9 +44,9 @@ def main():
                 print('Error getting info from endpoint {}'.format(endpoint))
                 continue
             try:    
-                if not info['head_block_producer'] in eoslbp:
-                    eoslbp[info['head_block_producer']] = {}
-                eoslbp[info['head_block_producer']]['last_block_produced_time'] = info['head_block_time']
+                if not info['head_block_producer'] in eoslpb:
+                    eoslpb[info['head_block_producer']] = {}
+                eoslpb[info['head_block_producer']]['last_block_produced_time'] = info['head_block_time']
             except:
                 print('Error getting head_block_producer from endpoint {}'.format(endpoint))
                 continue
@@ -56,8 +56,8 @@ def main():
             except:
                 print('Error getting producers from endpoint {}'.format(endpoint))
                 continue
-            eoslbp['producers'] = [ p['producer_name'] for p in producers ]
-            mpu.io.write(json_file, eoslbp)
+            eoslpb['producers'] = [ p['producer_name'] for p in producers ]
+            mpu.io.write(json_file, eoslpb)
             break
         time.sleep(5)
 
